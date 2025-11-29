02858fa4a6f6147a2dc4636cae8ac559564f95db
kurirohan <project.nuclear942@slmail.me>
2025-11-29 10:31:09 +0800

    Serve static PDF

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/',
   },
   devtool: "eval-source-map",
   devServer: {
@@ -17,6 +19,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [ 
+        { from: 'src/assets/attachments', to: 'attachments' },
+      ],
     }),
   ],
   module: {
@@ -32,7 +39,18 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      }
+      },
+      {
+        test: /\.pdf$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]', // Optional: customize output filename
+            },
+          },
+        ],
+      },
     ],
   },
 };
