478d40ea0ff653c5992d42091f7ea95ede267cc6
kurirohan <project.nuclear942@slmail.me>
2025-11-29 09:36:27 +0800

    Make favicon work

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import "../styles/styles.css"
 import { Module } from "./module.js";
 
-console.log("Dear God, dear God, tinkle-tinkle hoy!");
+// console.log("Dear God, dear God, tinkle-tinkle hoy!");
