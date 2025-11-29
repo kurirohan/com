df232a381287eacae598a56219c8c90351c950f9
kurirohan <project.nuclear942@slmail.me>
2025-11-29 11:42:54 +0800

    fix: publicPath

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: '/',
+    publicPath: '/com/',
   },
   devtool: "eval-source-map",
   devServer: {
